142c96ebb45602ce61d1f4217859bb4bb51ea0c4
sourav <souravchongrey@gmail.com>
2024-07-21 17:05:14 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+ setup(){
+  
+ }
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      status: 'pending'
+      link: 'https://github.com/gks77/icp'
+      task:[Task One, Task Two, Task Three]
+    };
+  },
+};
+</script>
+<template>
+  <h1>{{msg}}</h1>
+  <h1 v-if="status === 'active'">Hello</h1>
+  <h1 v-else-if="status === 'pending'">Bye</h1>
+  <h1 v-else>Hi</h1>
+</template>
+
